d1328ad3402de851876d5eb3ef0a726d7380a027
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2023-01-02 18:37:48 +0200

    addingModel

---
+++ Time/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using Time.Model;
+using Time.Services;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System.Linq;
+
+namespace Time
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TodoModel> _todoDateList;
+        private FileIOService _fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDateList = new BindingList<TodoModel>();
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _todoDateList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+           
+
+            dgTodoList.ItemsSource = _todoDateList;
+
+            _todoDateList.ListChanged += _todoDateList_ListChanged;
+
+            
+        }
+        
+        private void _todoDateList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+                
+            }
+            CheckTime();
+        }
+        private void CheckTime()
+        {
+            
+            TodoModel todoModelTmp = new TodoModel();
+            for(int i=0;i<_todoDateList.Count();i++)
+            {
+                todoModelTmp = _todoDateList[i];
+                if (todoModelTmp._finishDate == DateTime.Now)
+                {
+                    new ToastContentBuilder()
+                    .AddArgument("action", "viewConversation")
+                    .AddArgument("conversationId", 9813)
+                    .AddText("Andrew sent you a picture")
+                    .AddText("Check this out, The Enchantments in Washington!")
+                    .Show();
+                    break;
+                }
+                if (i == _todoDateList.Count() - 1)
+                    i = 0;
+            }
+            
+        }
+    }
+}
---
+++ Time/Model/TodoModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+
+namespace Time.Model
+{
+    class TodoModel: INotifyPropertyChanged
+    {
+        public  DateTime _createDate;
+
+        private string _text;
+        private bool _isDone;
+        public DateTime _finishDate;
+
+
+        public bool IsDone
+		{
+			get { return _isDone; }
+			set 
+			{
+				if (_isDone == value)
+					return;
+				_isDone = value;
+				OnPropertyChanged("IsDone");
+			}
+		}
+		
+
+		public string Text
+		{
+			get { return _text; }
+			set 
+			{
+				if (_text == value)
+					return;
+				_text = value;
+				OnPropertyChanged("Text");
+			}
+		}
+        public DateTime CreationDate
+        {
+            get { return _createDate; }
+            set
+            {
+                if (_createDate == value)
+                    return;
+                _createDate = value;
+                OnPropertyChanged("FinishDate");
+            }
+        }
+        public DateTime FinishDate
+		{
+			get { return _finishDate; }
+			set
+			{
+				if (_finishDate == value)
+					return;
+				_finishDate = value;
+				OnPropertyChanged("FinishDate");
+			}
+		}
+
+		
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void CheckTimeAndSendNotify()
+		{
+			while (true)
+			{
+				if(_finishDate == DateTime.Now)
+				{
+                    new ToastContentBuilder()
+					.AddArgument("action", "viewConversation")
+					.AddArgument("conversationId", 9813)
+					.AddText("Andrew sent you a picture")
+					.AddText("Check this out, The Enchantments in Washington!")
+					.Show();
+                }
+			}
+
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+    }
+}
